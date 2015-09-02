296e3306ac1345c16c3cc0b7317a62ca9d456036
Marco Krage <marco@my-azur.de>
2015-09-02 09:49:05 +0200

    rewrite row display calculation

--- jquery.tablePagination.js
+++ jquery.tablePagination.js
@@ -1,69 +1,91 @@
 /*
 	tablePagination Plugin for jQuery (by Marco Krage)
-	
-	Version: 0.1
-		
+	Version: 0.2
 	http://my-azur.de/blog/####
-	
+
 	This plugin is offered under the MIT license.
 	(c) 2014 by Marco Krage, http://marco.mit-license.org
 */
 (function ($) {
 
-    $.fn.tablePagination = function (options) {
+  $.fn.tablePagination = function (options) {
 
-        var settings = $.extend({
-            perPage: 20,
-            initPage: 1,
-            position: 'bottom',
-            paginationClass: 'tablePagination'
-        }, options);
+    var settings = $.extend({
+      perPage: 20,
+      initPage: 1,
+      position: 'bottom',
+      paginationClass: 'tablePagination',
+      showAllButton: true
+    }, options);
 
-        return this.each(function () {
-            var $table = $(this);
-            var $rows = $table.find('tr:has(td)');
+    return this.each(function () {
+      var $table = $(this);
+      var $rows = $table.find('tr:has(td)');
+      var pages;
 
-            if ($rows.length > settings.perPage) {
-                var pages = Math.ceil($rows.length / settings.perPage);
-                var pageCount = 1;
-                $rows.each(function (i) {
-                    if (i + 1 > settings.perPage * pageCount) {
-                        pageCount++;
-                    }
-                    $(this).attr('data-page', pageCount);
-                });
+      if ($rows.length > settings.perPage) {
+        pages = Math.ceil($rows.length / settings.perPage);
 
-                var $pageination = $('<ol/>').addClass(settings.paginationClass);
-                switch (settings.position) {
-                    case 'top':
-                        $pageination.insertBefore($table);
-                        break;
-                    case 'bottom':
-                    default:
-                        $pageination.insertAfter($table);
-                }
+        var $pageination = $('<ol/>').addClass(settings.paginationClass);
+        switch (settings.position) {
+          case 'top':
+            $pageination.insertBefore($table);
+            break;
+          case 'bottom':
+          default:
+            $pageination.insertAfter($table);
+        }
 
-                for (i = 1; i <= pages; i++) {
-                    var pageNumber = $('<li/>').attr('data-page', i).text(i).appendTo($pageination);
-                    pageNumber.click(function () {
-                        showPage($(this).attr('data-page'));
-                    });
-                }
-                showPage(settings.initPage);
-            }
+        var clickPagination = function( i ){
+          return function(){
+          $pageination.find('li').removeClass('current');
+          //$(this).addClass('current');
+          showPage(i);
+          }
+        }
 
-            function showPage(pageNum) {
-                if (pageNum < 1) {
-                    pageNum = 1;
-                }
-                if (pageNum > pages) {
-                    pageNum = pages;
-                }
-                $rows.hide();
-                $rows.filter('[data-page=' + pageNum + ']').show();
-                $pageination.find('li').removeClass('current');
-                $pageination.find('[data-page=' + pageNum + ']').addClass('current');
-            }
-        });
-    };
+        for (i = 1; i <= pages; i++) {
+          $('<li/>').text(i).click(clickPagination(i)).appendTo($pageination);
+        }
+
+        if(settings.showAllButton) {
+          $('<li/>').text("Alle").click(function () {
+            showPage(0);
+          }).appendTo($pageination);
+        }
+
+        showPage(settings.initPage);
+      }
+
+      function showPage(pageNum) {
+        pageNum = pageNum - 1; // working 0 based
+
+        $pageination.find('li').removeClass('current');
+        
+        if(pageNum == -1) { // for displaying all rows
+          $rows.show();
+          $pageination.filter(':last-child').addClass('current');
+          return;
+        }
+        if (pageNum < 0) {
+          pageNum = 0;
+        }
+        if (pageNum > pages) {
+          pageNum = pages;
+        }
+        
+        var from = pageNum;
+        var to = settings.perPage -1;
+        
+        if(pageNum > 0) {
+          from = from * settings.perPage;
+        }
+        
+        $pageination.find('li:eq(' + pageNum + ')').addClass('current');
+        
+        $rows.hide().filter(':eq(' + from + '), :gt(' + from + '):lt(' + to + ')').show();
+
+      }
+    });
+  };
 }(jQuery));
